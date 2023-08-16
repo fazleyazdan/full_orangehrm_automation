358a12a11c8b9f0af33c82b096a1cf32324028fa
yazdan777 <fazleyazdan345@gmail.com>
2023-08-16 15:54:35 +0500

    full Automation of OrangeHRM Login module. Automation of rediff , nopcommerce etc

---
+++ Day_8/explicit_wait.py
@@ -0,0 +1,57 @@
+from selenium import webdriver
+from selenium.common import NoSuchElementException, ElementNotVisibleException, ElementNotSelectableException
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# this will prevent the browser from automatically closing
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+ser_obj = Service("C:\Drivers\chromedriver_win32\chromedriver.exe")
+driver = webdriver.Chrome(options=options, service=ser_obj)
+
+# explicit wait works based on the condition and not on the time
+# explicit wait have 2 parts one is declaration and the other is utilization/usage
+# it takes 2 arguments in declaration 'driver' & time (in seconds)
+# we use webdriver for explicit wait
+
+# here I have used webDriverWait class and made an object of that class ie 'myWait'
+# we wil use the object further
+# myWait = WebDriverWait(driver, 10)  # basic explicit wait declaration
+
+# advanced declaration syntax
+myWait = WebDriverWait(driver, 10, poll_frequency=2, ignored_exceptions=[NoSuchElementException,
+                                                                         ElementNotVisibleException,
+                                                                         ElementNotSelectableException])
+# or instead of writing all those exceptions we can just write 'Exception' and it will handle all exceptions
+# as the  maximum time out is 10 secs. we use poll frequency to look for the element  after every 2 secs
+# if the element is located in the first cycle of p.frequency then the script will move on to next statements
+# if element is not found.then poll.frequency will look for the element 5 times in those 10 sec until it is located
+# poll frequency should always be less than maximum time out
+
+driver.get('https://www.google.com/')
+search_box = driver.find_element(By.NAME, 'q')
+search_box.send_keys('selenium')
+
+# if you want to click enter button from your keyboard we use 'submit'
+search_box.submit()
+
+# now if there is chance of synchronization problem
+# there is no need to use find_element because in explicit wait it is inclusive
+# there is a class called 'EC' (expected condition) we will use this with the object. you have to import 'EC'
+# here we are using the condition with EC. until that condition is satisfied/true the script will wait
+
+# cons:
+# 1: if the condition is not satisfied then after time that we have specified in declaration ....
+# will throw an exception , or we can handle the exception automatically by specifying them in declaration
+# 2: explicit wait is not a single statement you have to insert it multiple times
+
+# pros :
+# more effective because there is exception handling mechanism as well
+
+searchLink = myWait.until(EC.presence_of_element_located((By.XPATH, "//h3[text()='Selenium']")))
+searchLink.click()
+
+driver.quit()
---
+++ Day_8/implicit_wait.py
@@ -0,0 +1,64 @@
+# whenever you are requesting data from the server, the response may be delayed
+# so in that case synchronization problem may arise
+# now in a scenario like that it is a good practice to use wait commands specially implicit wait
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+
+# this will prevent the browser from automatically closing
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+ser_obj = Service("C:\Drivers\chromedriver_win32\chromedriver.exe")
+driver = webdriver.Chrome(options=options, service=ser_obj)
+
+# pros:
+# now implicit wait have to be used once in a script, and it is available for the works until the driver is alive
+# meaning it works for all the statement which comes after it.
+# where ever the synchronization problem may occur it will handle it
+# driver is only killed when you 'close' or 'quit' the driver. ie 'driver.quit'
+
+# if a response takes 5 sec to display an element. and we specified 10 sec to wait
+# in that case imp wait will use the 5 sec and as soon as the element is displayed it won't wait
+# and will jump to next statement resulting in speedy script and good performance
+
+# cons:
+# if the element is not available within the mentioned time there is chance of getting exception
+driver.implicitly_wait(10)  # max time wait standard is 10 sec
+
+driver.get('https://www.google.com/')
+search_box = driver.find_element(By.NAME, 'q')
+search_box.send_keys('selenium')
+
+# if you want to click enter button from your keyboard we use 'submit'
+search_box.submit()
+
+# we have searched for selenium and the server will show us the result
+# now there is a chance to get synchronization problem because the response from the server may be delayed
+
+# time.sleep(5)
+# now in this case we can use time.sleep(). it is not in selenium and is native to python only
+# you have to import it
+
+# cons:
+# 1: if the element is available within 2 seconds of the mentioned time, and
+# we have given 5 seconds then 3 seconds will be wasted due to which script time is increased
+# 2: if the element is not available within the mentioned time then there is chance of getting Exception
+# 3: you have to write it every time you think the synchronization problem may occur
+
+
+# Pros:
+# 1: simple to use
+# 2: Sometimes, a javascript function that change the inner text of the element which takes sometime.
+# In that case only hard wait can be used because the element is already present,
+# 'implicit wait and explicit wait will get the element before the inner text of the element
+# changes resulting in no errors but wrong results.
+
+# identify the link on the web which have text 'Selenium' and click. we created rel XPATH for that
+driver.find_element(By.XPATH, "//h3[text()='Selenium']").click()
+
+# if you want to catch exception or handle the exceptions automatically we use try catch block in imp.wait
+
+
